e2cab675ea61c2a4600270e94463a8e73f5ff56b
Sohel Rana <mdsohelrana191807@gmail.com>
2026-08-28 23:53:52 +0600

    ssss

---
+++ js/script.js
@@ -0,0 +1,55 @@
+
+let firstName = "sohel rana"
+console.log(firstName);
+
+
+
+let myNumber = 15
+if (myNumber >= 20) {
+    console.log("adult");
+}
+else {
+    console.log("Child");
+}
+
+
+
+let results =400
+
+
+
+if (results>100){
+    console.log("tom bohot chalak banda ho");
+}
+
+
+//  else if (results >=0 && results>=32){
+//     console.log("Fail");
+// }
+
+ else if (results >= 33 && results < 40) {
+    console.log(" grade ___ D");
+}
+
+else if (results >= 40 && results < 50) {
+    console.log("grade ___ C");
+}
+
+else if (results >= 50 && results < 60) {
+    console.log("grade ___ B");
+}
+
+else if (results >= 60 && results < 70) {
+    console.log("grade ___ A-");
+}
+
+else if (results >= 70 && results < 80) {
+    console.log("grade ___ A");
+}
+
+else if (results >= 80 && results < 100) {
+    console.log("grade ___ A+");
+}
+else{
+    console.log("tore jeno kono din aykhane na dekhi ");
+}
